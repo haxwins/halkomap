fbf284a7e3e55be4988d61ad8f90e28579572f4f
haxwins <haxregist@gmail.com>
2018-05-17 11:17:57 +0200

    1.0

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ window.onload = () =>{
     window.addEventListener('deviceorientation', (e)=>{
         let tiltLR = e.gamma;
         let tiltFB = e.beta;
-
+        document.getElementById('text1').innerHTML = tiltLR;
+        document.getElementById('text2').innerHTML = tiltFB;
         element.style.transform = `rotate(${tiltLR}dag) rotate3d(1,0,0, ${tiltFB * -1}deg)`;
     })
 }
