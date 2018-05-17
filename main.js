b1e985d87304b9b29a64bb86ad4a85d3fe059e01
haxwins <haxregist@gmail.com>
2018-05-17 11:58:13 +0200

    1.0

--- main.js
+++ main.js
@@ -10,11 +10,9 @@ window.onload = () =>{
      }
      
     
-    window.addEventListener('deviceorientation', (e)=>{
-        let tiltLR = e.gamma;
-        let tiltFB = e.betta;
-        document.getElementById('text3').innerHTML = 'we in';
-        document.getElementById('logo').style.transform = `rotate(${tiltLR}deg) rotate3d(1,0,0, ${tiltFB * -1}deg)`;
-    })
+     window.addEventListener('deviceorientation', function(event) {
+        document.getElementById('text3').innerHTML = "we in";
+        
+     });
     
 }
