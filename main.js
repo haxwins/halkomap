e2b30595feaec740e92362d68e66af4df505c31e
haxwins <haxregist@gmail.com>
2018-05-17 11:51:27 +0200

    1.0

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 window.onload = () =>{
     console.log("asd")
     let element = document.getElementById('logo')
-    element.style.width = "200px"; 
+    element.style.width = "200px";
+    if (window.DeviceOrientationEvent) {
+        document.getElementById('text1').innerHTML = 'can';
+        
+     } else {
+        document.getElementById('text2').innerHTML = 'cant';
+     }
+     
+     document.getElementById('logo').style.transform = `rotate(${tiltLR}deg) rotate3d(1,0,0, ${tiltFB * -1}deg)`;
     window.addEventListener('deviceorientation', (e)=>{
         let tiltLR = e.gamma;
-        let tiltFB = e.beta;
-        document.getElementById('text1').innerHTML = tiltLR;
-        document.getElementById('text2').innerHTML = tiltFB;
-        element.style.transform = `rotate(${tiltLR}dag) rotate3d(1,0,0, ${tiltFB * -1}deg)`;
+        let tiltFB = e.betta;
+        document.getElementById('text3').innerHTML = 'we in';
+        document.getElementById('logo').style.transform = `rotate(${tiltLR}deg) rotate3d(1,0,0, ${tiltFB * -1}deg)`;
     })
 }
